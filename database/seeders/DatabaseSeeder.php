d994229e66b5e566b76a67cdaa5ccfe8bfd7f652
jose98pp <jose98pp@users.noreply.github.com>
2026-09-16 21:51:41 -0400

    Update DatabaseSeeder to include site settings and editorial data seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminUserSeeder::class,
             CategorySeeder::class,
+            SiteSettingSeeder::class,
+            EditorialRealDataSeeder::class,
         ]);
     }
 }
